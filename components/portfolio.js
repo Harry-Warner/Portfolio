457489a5c45484d3f880436d8616479c2e08ad76
Harry-Warner <harry.e.p.warner@gmail.com>
2020-08-05 11:24:13 +0100

    portfolio section rearrange

--- components/portfolio.js
+++ components/portfolio.js
@@ -19,8 +19,8 @@ const Portfolio = () => {
           Portfolio
         </h1>
         <h2 className="text-xl text-dark text-center font-sans w-11/12 lg:w-3/4 mx-auto">
-          Below you will find my projects to date. Starting with two freelance
-          clients and finishing with a personal project of mine.
+          Below you will find my projects to date. Two freelance clients and a
+          personal project of mine.
         </h2>
         <Project
           left
@@ -29,31 +29,31 @@ const Portfolio = () => {
           github={"https://github.com/Harry-Warner/Veggiessimo-repo"}
           url={"https://veggiessimo.com.au"}
           techStack={
-            "React, Next JS, Sanity, Tailwind CSS, Styled-Components, GROQ"
+            "React, Next.js, Sanity, Tailwind CSS, Styled-Components, GROQ"
           }
           description={`Veggiessimo asked me to create a website for their recipes and posts on sustainability. Using React, Next JS and a Sanity headless CMS I was able to give them everything they needed and more. Through the sign up form I created they are now receiving more subscribers to their monthly emails.`}
         />
         <Project
-          right
-          project={"martin"}
-          title={"Martin Ross Pringle"}
-          github={"https://github.com/Harry-Warner/Martin-Ross-Pringle"}
-          url={"https://martinrosspringle.co.uk"}
-          techStack={"HTML, CSS, JavaScript"}
-          description={`My first voyage into web development came in the form of Martins portfolio. He had a specific design in mind that revolved around simplicity allowing his photos to stand out. I used vanilla Javascript along with HTML and CSS`}
-        />
-        <Project
           playlist
-          left
+          right
           internal
           url={"/publicplaylist"}
           github={"https://github.com/Harry-Warner/Portfolio"}
           project={"playlist"}
           title={"Music Playlist Project"}
           techStack={
-            "React, Next JS, Node JS, MongoDB, Tailwind CSS, Styled-Components"
+            "React, Next.js, Node.js, MongoDB, Tailwind CSS, Styled-Components"
           }
-          description={`Creating a database with MongoDB, I connected the API using Node JSwith API endpoints to read, create, update and delete songs from a playlist.`}
+          description={`Creating a database with MongoDB, I connected the API using Node.js with API endpoints to read, create, update and delete songs from a playlist.`}
+        />
+        <Project
+          left
+          project={"martin"}
+          title={"Martin Ross Pringle"}
+          github={"https://github.com/Harry-Warner/Martin-Ross-Pringle"}
+          url={"https://martinrosspringle.co.uk"}
+          techStack={"HTML, CSS, JavaScript"}
+          description={`My first voyage into web development came in the form of Martins portfolio. He had a specific design in mind that revolved around simplicity allowing his photos to stand out. I used vanilla Javascript along with HTML and CSS`}
         />
       </section>
     </>
